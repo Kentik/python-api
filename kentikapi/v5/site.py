9d6abe71c3d04967dff36e37be4fe85a207f0aea
Charlie Allom <yeled@github.com>
2020-05-25 19:45:09 +0100

    sites

---
+++ kentikapi/v5/site.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from builtins import str
+from builtins import object
+import json
+import requests
+
+
+"""Site API client"""
+
+
+class Site(object):
+    """Returns all Kentik Sites"""
+
+    def __init__(self, api_email, api_token, base_url='https://api.kentik.com'):
+        self.api_email = api_email
+        self.api_token = api_token
+        self.base_url = base_url
+
+    def list(self, url):
+        headers = {
+            'User-Agent': 'kentik-python-api/0.2',
+            'Content-Type': 'application/json',
+            'X-CH-Auth-Email': self.api_email,
+            'X-CH-Auth-API-Token': self.api_token
+        }
+        print(headers)
+        resp = requests.get(url, headers=headers)
+
+        # print the HTTP response to help debug
+        print(resp.text)
+
+        # break out at first sign of trouble
+        resp.raise_for_status()
+        sites = resp.json()
+
+        return sites
