5cd6e2fa54ba26d856d7e0e579313cac1b982539
Charlie Allom <yeled@github.com>
2020-05-25 21:27:09 +0100

    blackify

--- kentikapi/v5/device.py
+++ kentikapi/v5/device.py
@@ -13,33 +13,39 @@ import requests
 class Device(object):
     """Creates Kentik Device"""
 
-    def __init__(self, api_email, api_token, base_url='https://api.kentik.com'):
+    def __init__(self, api_email, api_token, base_url="https://api.kentik.com"):
         self.api_email = api_email
         self.api_token = api_token
         self.base_url = base_url
 
-    def create(self, host, host_ip, site_id, plan_id, community="public", url='https://api.kentik.com/api/v5/device'):
+    def create(
+        self,
+        host,
+        host_ip,
+        site_id,
+        plan_id,
+        community="public",
+        url="https://api.kentik.com/api/v5/device",
+    ):
         headers = {
-            'User-Agent': 'kentik-python-api/0.2',
-            'Content-Type': 'application/json',
-            'X-CH-Auth-Email': self.api_email,
-            'X-CH-Auth-API-Token': self.api_token
+            "User-Agent": "kentik-python-api/0.2",
+            "Content-Type": "application/json",
+            "X-CH-Auth-Email": self.api_email,
+            "X-CH-Auth-API-Token": self.api_token,
         }
         data = {
-            'device': {
-                "device_name": host.replace('.', '_'),
+            "device": {
+                "device_name": host.replace(".", "_"),
                 "device_type": "router",
                 "device_description": host,
-                "sending_ips": [
-                    host_ip
-                ],
+                "sending_ips": [host_ip],
                 "device_sample_rate": 4096,
                 "plan_id": plan_id,
                 "site_id": site_id,
                 "minimize_snmp": False,
                 "device_snmp_ip": host_ip,
                 "device_snmp_community": community,
-                "device_bgp_type": "none"
+                "device_bgp_type": "none",
             }
         }
         resp = requests.post(url, headers=headers, json=data)
--- kentikapi/v5/site.py
+++ kentikapi/v5/site.py
@@ -13,17 +13,17 @@ import requests
 class Site(object):
     """Returns all Kentik Sites"""
 
-    def __init__(self, api_email, api_token, base_url='https://api.kentik.com'):
+    def __init__(self, api_email, api_token, base_url="https://api.kentik.com"):
         self.api_email = api_email
         self.api_token = api_token
         self.base_url = base_url
 
-    def list(self, url='https://api.kentik.com/api/v5/sites'):
+    def list(self, url="https://api.kentik.com/api/v5/sites"):
         headers = {
-            'User-Agent': 'kentik-python-api/0.2',
-            'Content-Type': 'application/json',
-            'X-CH-Auth-Email': self.api_email,
-            'X-CH-Auth-API-Token': self.api_token
+            "User-Agent": "kentik-python-api/0.2",
+            "Content-Type": "application/json",
+            "X-CH-Auth-Email": self.api_email,
+            "X-CH-Auth-API-Token": self.api_token,
         }
         resp = requests.get(url, headers=headers)
 
